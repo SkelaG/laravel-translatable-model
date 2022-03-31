8db1815ead99ba023abddbb7f8a92ef7a171ba52
skelag <cugfug576@gmail.com>
2022-03-31 17:33:41 +0300

    updateOrCreate & firstOrCreate

--- src/Builders/TranslatableQueryBuilder.php
+++ src/Builders/TranslatableQueryBuilder.php
@@ -14,12 +14,23 @@ class TranslatableQueryBuilder extends \Illuminate\Database\Eloquent\Builder
         parent::__construct($query);
     }
 
-    public function where($column, $operator = null, $value = null, $boolean = 'and'): TranslatableQueryBuilder|\Illuminate\Database\Eloquent\Builder|\m
+    public function where($column, $operator = null, $value = null, $boolean = 'and'): TranslatableQueryBuilder|\Illuminate\Database\Eloquent\Builder
     {
         if (in_array($column, $this->translatable)) {
             return $this->whereHas('translation', function ($query) use ($column, $operator, $value, $boolean) {
                 $query->where($column, $operator, $value, $boolean);
             });
+        } elseif (is_array($column)) {
+            foreach ($column as  $key => $value) {
+                if (in_array($key, $this->translatable)) {
+                    $this->whereHas('translation', function ($query) use ($key, $value) {
+                        $query->where($key, $value);
+                    });
+                } else {
+                    $this->where($key, $value);
+                }
+            }
+            return $this;
         } else {
             return parent::where($column, $operator, $value, $boolean);
         }
