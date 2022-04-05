0dbcec1ca989cc2ec2b3bde3db5344dcf4e8826a
skelag <cugfug576@gmail.com>
2022-04-05 11:45:57 +0300

    migrations for translatable tables

--- src/Migrations/TranslatableMigration.php
+++ src/Migrations/TranslatableMigration.php
@@ -9,14 +9,22 @@ class TranslatableMigration extends \Illuminate\Database\Migrations\Migration
 {
     public function translates($main_table, $columns)
     {
-        Schema::table(substr($main_table, -1), function (Blueprint $table) use ($columns, $main_table) {
-            $table->id();
-            $table->foreign(substr($main_table, -1).'_id')->references('id')->on($main_table)->onDelete('cascade');
-            foreach ($columns as $column => $type) {
-                $table->{$type}($column);
-            }
-            $table->string('locale');
-            $table->timestamps();
-        });
+        Schema::create(mb_substr($main_table, 0, -1).'_translations',
+            function (Blueprint $table) use ($columns, $main_table) {
+                $foreign = mb_substr($main_table, 0, -1).'_id';
+                $table->id();
+                $table->unsignedBigInteger($foreign)->index();
+                $table->foreign($foreign)->references('id')->on($main_table)->onDelete('cascade');
+                foreach ($columns as $column => $type) {
+                    $table->{$type}($column);
+                }
+                $table->string('locale');
+                $table->timestamps();
+            });
+    }
+
+    public function dropTranslates($table)
+    {
+        Schema::dropIfExists(mb_substr($table, 0, -1).'_translations');
     }
 }
